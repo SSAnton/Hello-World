dc258639f657be09f12a84239a4481998ebfc830
Anton Silantev <silantev.anton.sergeevich@gmail.com>
2023-02-12 01:14:56 +0400

    add random Sum

--- Exampel003_Sum/Program.cs
+++ Exampel003_Sum/Program.cs
@@ -1,4 +1,6 @@
-﻿int namberA = 30;
-int namberB = 5;
-int result = namberA + namberB;
+﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
 Console.WriteLine(result);
