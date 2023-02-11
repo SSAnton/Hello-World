ecd5b142d48963612cfe5d1d09a462940b2136a7
Anton Silantev <silantev.anton.sergeevich@gmail.com>
2023-02-12 01:36:57 +0400

    add Exampel 005 Condition If Else

---
+++ Exampel005_Condition_If_Else/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите ваше имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша") 
+{
+   Console.WriteLine("Ура, это Маша!");
+}
+else
+{
+    Console.Write("Приыет,  ");
+    Console.WriteLine(username);
+}
